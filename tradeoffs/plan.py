60b0f1fd97f043553edd4dde93adda38e2a06111
alecramsay <4155251+alecramsay@users.noreply.github.com>
2024-01-11 07:59:25 -0800

    WIP

--- tradeoffs/plan.py
+++ tradeoffs/plan.py
@@ -2,9 +2,9 @@
 EVOLVING PLAN
 """
 
-from typing import List, Dict, Set, NamedTuple
+from typing import Any, List, Dict, Set, Tuple, NamedTuple, TypeAlias
 
-from rdabase import Assignment, OUT_OF_STATE
+from rdabase import Assignment, OUT_OF_STATE, write_csv
 from rdaensemble.general import make_plan
 
 """
@@ -14,23 +14,31 @@ class Assignment(NamedTuple):
 """
 Feature = Assignment
 
+District: TypeAlias = int | str
+FeatureOffsets: TypeAlias = Set[int]
+
+
+class BorderSegment(NamedTuple):
+    features: Dict[District, FeatureOffsets]  # Two: one for each side/district
+
 
 class EvolvingPlan:
     """An ensemble plan that can be evolved."""
 
     _features: List[Feature]
-    _feature_index: Dict[str, int]
+    _features_index: Dict[str, int]
     _features_by_district: Dict[str | int, Set[int]]
-    _indexed_graph: Dict[int, List[int]]
+    _features_graph: Dict[int, List[int]]
+    _border_segments: Dict[Tuple[District, District], BorderSegment]
 
     def __init__(
         self, district_by_geoid: Dict[str, int | str], graph: Dict[str, List[str]]
     ) -> None:
         assignments: List[Assignment] = make_plan(district_by_geoid)
         self._features = assignments
-        self._feature_index = {f.geoid: i for i, f in enumerate(assignments)}
+        self._features_index = {f.geoid: i for i, f in enumerate(assignments)}
         self._features_by_district = self.invert_plan()
-        self._indexed_graph = self.index_graph(graph)
+        self._features_graph = self.index_graph(graph)
 
     def invert_plan(self):
         """Collect geoids by district."""
@@ -38,7 +46,7 @@ class EvolvingPlan:
         inverted: Dict[str | int, Set[int]] = dict()
 
         for i, f in enumerate(self._features):
-            offset: int = self._feature_index[f.geoid]
+            offset: int = self._features_index[f.geoid]
             district: int | str = f.district
 
             if district not in inverted:
@@ -56,12 +64,21 @@ class EvolvingPlan:
         for geoid, neighbors in graph.items():
             if geoid == OUT_OF_STATE:
                 continue
-            offset: int = self._feature_index[geoid]
+            offset: int = self._features_index[geoid]
             indexed_graph[offset] = [
-                self._feature_index[n] for n in neighbors if n != OUT_OF_STATE
+                self._features_index[n] for n in neighbors if n != OUT_OF_STATE
             ]
 
         return indexed_graph
+
+    def to_csv(self, plan_path: str) -> None:
+        """Write the plan to a CSV."""
+
+        plan: List[Dict[str, str | int]] = [
+            {"GEOID": a.geoid, "DISTRICT": a.district} for a in self._features
+        ]
+
+        write_csv(plan_path, plan, ["GEOID", "DISTRICT"])
 
     # TODO - More ...
 
