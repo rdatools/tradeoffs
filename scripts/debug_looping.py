498b3fbc230150ce73032e4a5c1c66e895fb631d
alecramsay <4155251+alecramsay@users.noreply.github.com>
2024-01-19 08:24:33 -0800

    WIP

--- scripts/debug_looping.py
+++ scripts/debug_looping.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-DEBUGGING SCRIPT
+DEBUGG LOOPING - The Balzer looping
 """
 
 from typing import Any, List, Dict, NamedTuple
--- scripts/debug_measuring.py
+++ scripts/debug_measuring.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-DEBUGGING SCRIPT
+DEBUG MEASURING - The measuring of proposed moves
 """
 
 from typing import Any, List, Dict, NamedTuple
