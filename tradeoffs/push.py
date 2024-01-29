99e55f9aaea83c319f97cb3b06d61b3d266863a3
alecramsay <4155251+alecramsay@users.noreply.github.com>
2024-01-29 05:31:10 -0800

    WIP

--- tradeoffs/push.py
+++ tradeoffs/push.py
@@ -30,10 +30,6 @@ def push_point(
     ]  # Swap/mutation generators
 
     for generator in generators:
-        if debug:
-            print()
-            print(f"Generator: {generator.__name__}")
-
         npass: int = 1
         while True:
             if npass > limit:
@@ -88,23 +84,11 @@ def sweep_once(
 
     random_adjacent_districts: List[BorderKey] = plan.random_adjacent_districts()
 
-    if debug:
-        print()
-        print(f"# pairs of adjacent districts: {len(random_adjacent_districts)}")
-
     stable: bool = True
     for seg_key in random_adjacent_districts:
         mutations: List[Mutation] = plan.random_mutations(seg_key, generator)
         tried: int = 0
         valid_and_better: int = 0
-
-        if debug:
-            d1, d2 = seg_key
-            d1_id: DistrictID = plan.district_ids[d1]
-            d2_id: DistrictID = plan.district_ids[d2]
-
-            print()
-            print(f"{len(mutations)} mutations {d1}/{d1_id} <-> {d2}/{d2_id}:")
 
         for m in mutations:
             tried += 1
@@ -122,24 +106,11 @@ def sweep_once(
                     valid_and_better += 1
                     stable = False
 
-                    if debug:
-                        print("... Valid mutation the plan better!")
-
                     if verbose:
                         print(f"Nudged #'s:   {dimensions} = {prev_measures}", end="\r")
 
             if not valid or not better:
                 plan.undo()
-
-            if debug:
-                print()
-                print(f"... # remaining mutations: {len(mutations) - tried}")
-
-        if debug:
-            print(
-                f"... Summary: {valid_and_better} of {tried} mutations tried were valid."
-            )
-            print()
 
     plan.generation += 1
 
