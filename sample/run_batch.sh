3bbd15efa40147fe7a89dc8c3abc09e45e98edde
alecramsay <4155251+alecramsay@users.noreply.github.com>
2024-02-03 06:36:26 -0800

    WIP

---
+++ sample/run_batch.sh
@@ -0,0 +1,150 @@
+scripts/push_plan.py \
+--state NC \
+--plan sample/sample_plan.csv \
+--dimensions proportionality competitiveness \
+--seed 518 \
+--multiplier 1 \
+--prefix sample_proportionality_competitiveness_plan \
+--data ../rdabase/data/NC/NC_2020_data.csv \
+--shapes ../rdabase/data/NC/NC_2020_shapes_simplified.json \
+--graph ../rdabase/data/NC/NC_2020_graph.json \
+--output sample \
+--log sample/sample_proportionality_competitiveness_log.txt \
+--verbose \
+--no-debug
+
+scripts/push_plan.py \
+--state NC \
+--plan sample/sample_plan.csv \
+--dimensions proportionality minority \
+--seed 519 \
+--multiplier 1 \
+--prefix sample_proportionality_minority_plan \
+--data ../rdabase/data/NC/NC_2020_data.csv \
+--shapes ../rdabase/data/NC/NC_2020_shapes_simplified.json \
+--graph ../rdabase/data/NC/NC_2020_graph.json \
+--output sample \
+--log sample/sample_proportionality_minority_log.txt \
+--verbose \
+--no-debug
+
+scripts/push_plan.py \
+--state NC \
+--plan sample/sample_plan.csv \
+--dimensions proportionality compactness \
+--seed 520 \
+--multiplier 1 \
+--prefix sample_proportionality_compactness_plan \
+--data ../rdabase/data/NC/NC_2020_data.csv \
+--shapes ../rdabase/data/NC/NC_2020_shapes_simplified.json \
+--graph ../rdabase/data/NC/NC_2020_graph.json \
+--output sample \
+--log sample/sample_proportionality_compactness_log.txt \
+--verbose \
+--no-debug
+
+scripts/push_plan.py \
+--state NC \
+--plan sample/sample_plan.csv \
+--dimensions proportionality splitting \
+--seed 521 \
+--multiplier 1 \
+--prefix sample_proportionality_splitting_plan \
+--data ../rdabase/data/NC/NC_2020_data.csv \
+--shapes ../rdabase/data/NC/NC_2020_shapes_simplified.json \
+--graph ../rdabase/data/NC/NC_2020_graph.json \
+--output sample \
+--log sample/sample_proportionality_splitting_log.txt \
+--verbose \
+--no-debug
+
+scripts/push_plan.py \
+--state NC \
+--plan sample/sample_plan.csv \
+--dimensions competitiveness minority \
+--seed 522 \
+--multiplier 1 \
+--prefix sample_competitiveness_minority_plan \
+--data ../rdabase/data/NC/NC_2020_data.csv \
+--shapes ../rdabase/data/NC/NC_2020_shapes_simplified.json \
+--graph ../rdabase/data/NC/NC_2020_graph.json \
+--output sample \
+--log sample/sample_competitiveness_minority_log.txt \
+--verbose \
+--no-debug
+
+scripts/push_plan.py \
+--state NC \
+--plan sample/sample_plan.csv \
+--dimensions competitiveness compactness \
+--seed 523 \
+--multiplier 1 \
+--prefix sample_competitiveness_compactness_plan \
+--data ../rdabase/data/NC/NC_2020_data.csv \
+--shapes ../rdabase/data/NC/NC_2020_shapes_simplified.json \
+--graph ../rdabase/data/NC/NC_2020_graph.json \
+--output sample \
+--log sample/sample_competitiveness_compactness_log.txt \
+--verbose \
+--no-debug
+
+scripts/push_plan.py \
+--state NC \
+--plan sample/sample_plan.csv \
+--dimensions competitiveness splitting \
+--seed 524 \
+--multiplier 1 \
+--prefix sample_competitiveness_splitting_plan \
+--data ../rdabase/data/NC/NC_2020_data.csv \
+--shapes ../rdabase/data/NC/NC_2020_shapes_simplified.json \
+--graph ../rdabase/data/NC/NC_2020_graph.json \
+--output sample \
+--log sample/sample_competitiveness_splitting_log.txt \
+--verbose \
+--no-debug
+
+scripts/push_plan.py \
+--state NC \
+--plan sample/sample_plan.csv \
+--dimensions minority compactness \
+--seed 525 \
+--multiplier 1 \
+--prefix sample_minority_compactness_plan \
+--data ../rdabase/data/NC/NC_2020_data.csv \
+--shapes ../rdabase/data/NC/NC_2020_shapes_simplified.json \
+--graph ../rdabase/data/NC/NC_2020_graph.json \
+--output sample \
+--log sample/sample_minority_compactness_log.txt \
+--verbose \
+--no-debug
+
+scripts/push_plan.py \
+--state NC \
+--plan sample/sample_plan.csv \
+--dimensions minority splitting \
+--seed 526 \
+--multiplier 1 \
+--prefix sample_minority_splitting_plan \
+--data ../rdabase/data/NC/NC_2020_data.csv \
+--shapes ../rdabase/data/NC/NC_2020_shapes_simplified.json \
+--graph ../rdabase/data/NC/NC_2020_graph.json \
+--output sample \
+--log sample/sample_minority_splitting_log.txt \
+--verbose \
+--no-debug
+
+scripts/push_plan.py \
+--state NC \
+--plan sample/sample_plan.csv \
+--dimensions compactness splitting \
+--seed 527 \
+--multiplier 1 \
+--prefix sample_compactness_splitting_plan \
+--data ../rdabase/data/NC/NC_2020_data.csv \
+--shapes ../rdabase/data/NC/NC_2020_shapes_simplified.json \
+--graph ../rdabase/data/NC/NC_2020_graph.json \
+--output sample \
+--log sample/sample_compactness_splitting_log.txt \
+--verbose \
+--no-debug
+
---
+++ scripts/make_sample_jobs.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+"""
+MAKE SAMPLE "PUSH" JOBS
+
+For documentation, type:
+
+$ scripts/make_sample_jobs.py
+
+"""
+
+import itertools
+
+from rdaensemble.general import ratings_dimensions
+
+
+def main() -> None:
+    """Make sample 'push_plan.py' jobs."""
+
+    xx: str = "NC"
+    plan: str = "sample/sample_plan.csv"
+    seed: int = 518
+    multiplier: int = 1
+    data: str = "../rdabase/data/NC/NC_2020_data.csv"
+    shapes: str = "../rdabase/data/NC/NC_2020_shapes_simplified.json"
+    graph: str = "../rdabase/data/NC/NC_2020_graph.json"
+    output: str = "sample"
+
+    ratings_pairs: List = list(itertools.combinations(ratings_dimensions, 2))
+    for pair in ratings_pairs:
+        dimensions: str = " ".join(pair)
+        prefix: str = f"sample_{'_'.join(pair)}_plan"
+        logfile: str = f"{output}/sample_{'_'.join(pair)}_log.txt"
+
+        print(f"scripts/push_plan.py \\")
+        print(f"--state {xx} \\")
+        print(f"--plan {plan} \\")
+        print(f"--dimensions {dimensions} \\")
+        print(f"--seed {seed} \\")
+        print(f"--multiplier {multiplier} \\")
+        print(f"--prefix {prefix} \\")
+        print(f"--data ../rdabase/data/{xx}/{xx}_2020_data.csv \\")
+        print(f"--shapes ../rdabase/data/{xx}/{xx}_2020_shapes_simplified.json \\")
+        print(f"--graph ../rdabase/data/{xx}/{xx}_2020_graph.json \\")
+        print(f"--output {output} \\")
+        print(f"--log {logfile} \\")
+        print(f"--verbose \\")
+        print(f"--no-debug")
+        print()
+
+        seed += 1
+
+
+if __name__ == "__main__":
+    main()
+
+### END ###
