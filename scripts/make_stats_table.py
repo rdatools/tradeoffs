382ad77df939a7427b0d8ba4c0b2cf73e1e7e41a
alecramsay <4155251+alecramsay@users.noreply.github.com>
2024-02-15 11:45:35 -0800

    Added make_stats_table.py script

---
+++ scripts/make_stats_table.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+
+"""
+MAKE STATISTICS TABLE
+
+For example:
+
+$ scripts/make_stats_table.py \
+--scores ../../iCloud/fileout/ensembles/NC20C_ReCom_10K_scores.csv \
+--output ../../iCloud/fileout/artifacts/NC20C_ReCom_10K_statistics.csv \
+--no-debug
+
+For documentation, type:
+
+$ scripts/make_stats_table.py
+"""
+
+import argparse
+from argparse import ArgumentParser, Namespace
+
+from typing import List, Dict, Any, Callable
+
+import pandas as pd
+
+from rdabase import require_args, write_csv
+from rdaensemble.general import ratings_dimensions
+
+from tradeoffs import scores_to_df
+
+
+def main() -> None:
+    """ "Make historgrams for the ratings for the plans in an ensemble."""
+
+    args: argparse.Namespace = parse_args()
+
+    fieldnames: List[str] = ["map"] + ratings_dimensions
+    fieldtypes: List[Callable] = [str, int, int, int, int, int]
+
+    df: pd.DataFrame = scores_to_df(args.scores, fieldnames, fieldtypes)
+
+    cols: List[str] = ["count", "mean", "std", "min", "25%", "50%", "75%", "max"]
+    rows: List[Dict[str, Any]] = []
+    for dimension in ratings_dimensions:
+        df_stats: Dict = df[dimension].describe().to_dict()
+        stats: Dict = {}
+
+        for k, v in df_stats.items():
+            stats[k.upper()] = int(v) if k != "std" else v
+
+        row: Dict[str, Any] = {}
+        row["DIMENSION"] = dimension.capitalize()
+        row.update(stats)
+        rows.append(row)
+
+    write_csv(args.output, rows, ["DIMENSION"] + [c.upper() for c in cols])
+
+
+def parse_args():
+    parser: ArgumentParser = argparse.ArgumentParser(
+        description="Make historgrams for the ratings for the plans in an ensemble."
+    )
+
+    parser.add_argument(
+        "--scores",
+        type=str,
+        help="A CSV ensemble of scores including ratings to plot",
+    )
+    parser.add_argument(
+        "--output",
+        type=str,
+        help="The CSV file to write the ratings to",
+    )
+
+    parser.add_argument(
+        "-v", "--verbose", dest="verbose", action="store_true", help="Verbose mode"
+    )
+
+    # Enable debug/explicit mode
+    parser.add_argument("--debug", default=True, action="store_true", help="Debug mode")
+    parser.add_argument(
+        "--no-debug", dest="debug", action="store_false", help="Explicit mode"
+    )
+
+    args: Namespace = parser.parse_args()
+
+    # Default values for args in debug mode
+    debug_defaults: Dict[str, Any] = {
+        "scores": "../../iCloud/fileout/ensembles/NC20C_ReCom_10K_scores.csv",
+        "output": "../../iCloud/fileout/data/NC20C_ReCom_10K_statistics.csv",
+    }
+    args = require_args(args, args.debug, debug_defaults)
+
+    return args
+
+
+if __name__ == "__main__":
+    main()
+
+### END ###
