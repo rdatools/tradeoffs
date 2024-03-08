a81801421c25f48cd60701dfb756334fb358cfd7
alecramsay <4155251+alecramsay@users.noreply.github.com>
2024-03-08 07:28:34 -0800

    WIP

--- docs/_pages/changelog.markdown
+++ docs/_pages/changelog.markdown
@@ -6,6 +6,7 @@ permalink: changelog/
 
 This page tracks major changes to the site, in reverse chronological order:
 
+- Added deploy script
 - Annotated the state page template
 - Updated the backend of the pipeline
 - ...
---
+++ scripts/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Copy the artifacts for a state from fileout to docs
+#
+# For example:
+#
+# scripts/deploy.sh NC
+
+XX=$1
+FROM_DIR=../../iCloud/fileout
+TO_DIR=docs
+
+cp $FROM_DIR/images/$XX*.svg $TO_DIR/assets/images
+cp $FROM_DIR/_data/$XX*.csv $TO_DIR/_data
