3e29db67245c4fd0a2cfa180ba5b59d6d0efb820
alecramsay <4155251+alecramsay@users.noreply.github.com>
2024-03-09 06:05:08 -0800

    Restored find_frontiers.py

--- scripts/find_frontiers.py
+++ scripts/find_frontiers.py
@@ -19,7 +19,7 @@ $ scripts/find_frontiers.py
 
 import argparse
 from argparse import ArgumentParser, Namespace
-from typing import Any, List, Dict, Tuple, Set, Callable
+from typing import Any, List, Dict, Callable
 
 import warnings
 
@@ -32,7 +32,6 @@ from rdabase import require_args, read_json, write_json
 from rdaensemble.general import ratings_dimensions
 from tradeoffs import (
     scores_to_df,
-    read_ratings,
     find_frontiers,
     is_pareto_efficient_value,
     id_most_notable_maps,
@@ -49,60 +48,23 @@ def main() -> None:
     fieldnames: List[str] = ["map"] + ratings_dimensions
     fieldtypes: List[Callable] = [str, int, int, int, int, int]
 
-    ratings_df: pd.DataFrame = scores_to_df(
+    ratings: pd.DataFrame = scores_to_df(
         args.scores,
         fieldnames,
         fieldtypes,
         filter=True,
         verbose=args.verbose,
     )
-    ratings: List[Dict] = read_ratings(
-        args.scores,
-        verbose=args.verbose,
-    )
     metadata: Dict[str, Any] = read_json(args.metadata)
 
     frontiers: Dict[str, Any] = find_frontiers(
-        ratings_df, is_pareto_efficient_value, verbose=args.verbose
+        ratings, is_pareto_efficient_value, verbose=args.verbose
     )
-    # indices: List[Dict[str, Dict[str, str | int]]] = id_most_notable_maps(frontiers)
+    indices: List[Dict[str, Dict[str, str | int]]] = id_most_notable_maps(frontiers)
 
     output: Dict[str, Any] = metadata
     output["frontiers"] = frontiers
     # output["notable_maps"] = indices
-
-    #
-    print(args.zone)
-
-    zones: Dict[str, List[str]] = {}
-    frontier_points: Dict[str, Set[Tuple[int, int]]] = {}
-    for k, v in frontiers.items():
-        zones[k] = []
-        frontier_points[k] = set()
-
-        pair: List[str] = k.split("_")
-        ydim: str = pair[0]
-        xdim: str = pair[1]
-        d1: int = ratings_dimensions.index(ydim)
-        d2: int = ratings_dimensions.index(xdim)
-
-        for m in v:
-            # zones[k].append(m["map"])
-            x: int = m["ratings"][d2]
-            y: int = m["ratings"][d1]
-            frontier_points[k].add((x, y))
-
-    # TODO - Find the plans in the zone
-    # For each plan in ratings:
-    #     For each frontier (pair of dimensions:
-    #         Get the pair of ratings
-    #         If they are "near" any frontier point, add it to that zone
-    # Print # of frontier points
-    # Print # of zone points
-
-    #
-
-    output["zones"] = zones
 
     if args.verbose:
         ratings_pairs: List = list(itertools.combinations(ratings_dimensions, 2))
@@ -141,7 +103,6 @@ def parse_args():
         type=str,
         help="Frontier maps JSON file",
     )
-    parser.add_argument("--zone", type=int, default=0, help="How wide a 'zone' to find")
 
     parser.add_argument(
         "-v", "--verbose", dest="verbose", action="store_true", help="Verbose mode"
@@ -157,10 +118,9 @@ def parse_args():
 
     # Default values for args in debug mode
     debug_defaults: Dict[str, Any] = {
-        "scores": "../../iCloud/fileout/ensembles/NC20C_scores.csv",
-        "metadata": "../../iCloud/fileout/ensembles/NC20C_scores_metadata.json",
+        "scores": "testdata/test_scores.csv",  # Only has map name & ratings
+        "metadata": "testdata/test_scores_metadata.json",
         "frontier": "~/Downloads/test_frontier.json",
-        "zone": 5,
         "verbose": True,
     }
     args = require_args(args, args.debug, debug_defaults)
