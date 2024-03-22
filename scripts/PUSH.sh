e93cf71eae13814872b408bf67e8ba91c27dfb6c
alecramsay <4155251+alecramsay@users.noreply.github.com>
2024-03-22 16:28:06 -0700

    WIP

--- scripts/PUSH.sh
+++ scripts/PUSH.sh
@@ -7,7 +7,7 @@
 # scripts/SETUP.sh NC
 
 XX=$1
-FROM_DIR=/Users/alecramsay/iCloud/fileout/hpc_dropbox
+FROM_DIR=/Users/alecramsay/iCloud/fileout/tradeoffs
 TO_DIR=alecr@filexfer.hpc.arizona.edu:./dropbox
 
 rsync -avz $FROM_DIR/$XX/ $TO_DIR/$XX
