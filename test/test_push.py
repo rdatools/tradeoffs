0573153440aec83f3d4af999740bd3f42061268c
alecramsay <4155251+alecramsay@users.noreply.github.com>
2024-03-25 15:19:28 -0700

    WIP

--- test/test_push.py
+++ test/test_push.py
@@ -11,20 +11,23 @@ from testutils import *
 class TestCompareFunctions:
     def test_two_dimensions(self) -> None:
         is_better = make_better_fn()
-        assert is_better((0.0, 0.0), (0.1, 0.0)) is True
-        assert is_better((0.0, 0.0), (0.0, 0.1)) is True
-        assert is_better((0.0, 0.0), (0.1, 0.1)) is True
-        assert is_better((0.0, 0.0), (0.0, 0.0)) is False
+        assert is_better((50.0, 50.0), (51.0, 50.0)) is True
+        assert is_better((50.0, 50.0), (50.0, 51.0)) is True
+        assert is_better((50.0, 50.0), (51.0, 51.0)) is True
+        assert is_better((50.0, 50.0), (50.0, 50.0)) is False
+        assert is_better((50.0, 50.0), (49.0, 50.0)) is False
+        assert is_better((50.0, 50.0), (50.0, 49.0)) is False
+        assert is_better((50.0, 50.0), (49.0, 49.0)) is False
 
     def test_one_dimension(self) -> None:
-        is_better = make_better_fn(constrain=0, anchor=1.0)
-        assert is_better((1.0, 1.0), (1.0, 1.0)) is False
-        assert is_better((1.0, 1.0), (1.0, 1.1)) is True
-        assert is_better((1.0, 1.0), (1.005, 1.1)) is True
-        assert is_better((1.0, 1.0), (0.995, 1.1)) is True
+        is_better = make_better_fn(constrain=0, anchor=50.0, tolerance=0.01)
+        assert is_better((50.0, 50.0), (50.0, 50.0)) is False
+        assert is_better((50.0, 50.0), (50.0, 51.0)) is True
+        assert is_better((50.0, 50.0), (50.1, 51.0)) is True
+        assert is_better((50.0, 50.0), (49.9, 51.0)) is True
 
-        assert is_better((1.0, 1.0), (1.015, 1.1)) is False
-        assert is_better((1.0, 1.0), (0.985, 1.1)) is False
+        assert is_better((50.0, 50.0), (52.0, 51.0)) is False
+        assert is_better((50.0, 50.0), (48.0, 51.0)) is False
 
 
 ### END ###
