925c14bed76be48231be1501864b105046080a79
alecramsay <4155251+alecramsay@users.noreply.github.com>
2024-03-26 14:07:40 -0700

    WIP

--- scripts/PUSH.sh
+++ scripts/PUSH.sh
@@ -10,4 +10,7 @@ XX=$1
 FROM_DIR=/Users/alecramsay/iCloud/fileout/tradeoffs
 TO_DIR=alecr@filexfer.hpc.arizona.edu:./dropbox
 
-rsync -avz $FROM_DIR/$XX/ $TO_DIR/$XX
+rsync -avz $FROM_DIR/$XX/data $TO_DIR/$XX
+rsync -avz $FROM_DIR/$XX/jobs $TO_DIR/$XX
+rsync -avz $FROM_DIR/$XX/plans $TO_DIR/$XX
+cp $FROM_DIR/$XX/*.sh $TO_DIR/$XX
