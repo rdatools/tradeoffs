78ed2278ecbe415d1b9ae864e1f3dbffa187b521
alecramsay <4155251+alecramsay@users.noreply.github.com>
2024-03-27 06:49:38 -0700

    WIP

--- scripts/PUSH.sh
+++ scripts/PUSH.sh
@@ -5,11 +5,13 @@
 # For example:
 #
 # scripts/PUSH.sh NC
-#
-# TODO - Limit what directories are pushed to the cluster somehow
 
 XX=$1
 FROM_DIR=/Users/alecramsay/iCloud/fileout/tradeoffs
 TO_DIR=alecr@filexfer.hpc.arizona.edu:./dropbox
 
-rsync -avz $FROM_DIR/$XX/ $TO_DIR/$XX
+exclude_docs="'$FROM_DIR/$XX/docs/*'" 
+exclude_ensembles="'$FROM_DIR/$XX/ensembles/*'" 
+exclude_pushed="'$FROM_DIR/$XX/pushed/*'"
+
+rsync -avz $FROM_DIR/$XX/ $TO_DIR/$XX --exclude $exclude_docs --exclude $exclude_ensembles --exclude $exclude_pushed
