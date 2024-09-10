5d9c1fd4a463f9590bec3cbd3ec2c80e7a196280
alecramsay <4155251+alecramsay@users.noreply.github.com>
2024-09-10 08:00:54 -0700

    WIP

--- scripts/SETUP.sh
+++ scripts/SETUP.sh
@@ -15,6 +15,8 @@ mkdir $ROOT_DIR/$XX
 # mkdir $ROOT_DIR/$XX/data
 mkdir $ROOT_DIR/$XX/docs
 mkdir $ROOT_DIR/$XX/ensembles
+mkdir $ROOT_DIR/$XX/ensembles-upper
+mkdir $ROOT_DIR/$XX/ensembles-lower
 # mkdir $ROOT_DIR/$XX/plans
 # mkdir $ROOT_DIR/$XX/jobs
 # mkdir $ROOT_DIR/$XX/pushed
