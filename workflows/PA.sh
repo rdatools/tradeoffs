8612bf853876c2daeeb71b4f1bdf72a705bd7bcb
alecramsay <4155251+alecramsay@users.noreply.github.com>
2024-09-10 18:14:59 -0700

    Updated make_workflow script

---
+++ workflows/PA.sh
@@ -0,0 +1,44 @@
+### Workflows for PA ###
+
+# Make sure you have done Steps 1-3 in the overall [workflow description](https://rdatools.github.io/tradeoffs/workflow)
+
+
+## Commands for 'congress'
+
+# From 'rdaensemble'
+scripts/STEP_5.sh --state PA --plan-type congress
+
+# From 'rdaensemble'
+scripts/STEP_6.sh --state PA --plan-type congress
+
+# From 'tradeoffs'
+scripts/STEP_7.sh --state PA --plan-type congress
+
+
+## Commands for 'upper'
+
+# From 'rdaensemble'
+scripts/STEP_5.sh --state PA --plan-type upper
+
+# From 'rdaensemble'
+scripts/STEP_6.sh --state PA --plan-type upper
+
+# From 'tradeoffs'
+scripts/STEP_7.sh --state PA --plan-type upper
+
+
+## Commands for 'lower'
+
+# From 'rdaensemble'
+scripts/STEP_5.sh --state PA --plan-type lower
+
+# From 'rdaensemble'
+scripts/STEP_6.sh --state PA --plan-type lower
+
+# From 'tradeoffs'
+scripts/STEP_7.sh --state PA --plan-type lower
+
+
+# Make sure you do Steps 8 & 9 in the overall [workflow description](https://rdatools.github.io/tradeoffs/workflow)
+
+### END ###
