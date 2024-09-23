fa2e12b74c5a3198969878694b9ec60cc004d824
alecramsay <4155251+alecramsay@users.noreply.github.com>
2024-09-23 07:37:50 -0700

    WIP

--- scripts-1time/duplicate_notable_maps.py
+++ scripts-1time/duplicate_notable_maps.py
@@ -5,30 +5,36 @@ Duplicate Notable maps.
 
 For example:
 
-$ scripts/duplicate_notable_maps.py
+$ scripts-1time/duplicate_notable_maps.py
 
 For documentation, type:
 
-$ scripts/duplicate_notable_maps.py -h
+$ scripts-1time/duplicate_notable_maps.py -h
 
 """
 
-import os
-
-from pg import *
+from rdabase import plan_type, yyyy
+from constants import NOTABLE_MAPS
 
 group: str = "Notable"
 label: str = "PG-" + group.upper()
 
 
 def make_command(group: str, label: str, xx: str, id: str) -> str:
-    # scripts/duplicate_map.sh NC Congress 2022 Proportional PG-NOTABLE 7cfcec86-b8bf-41fa-a6c2-0be9b3799747
-    return f"scripts/duplicate_map.sh {xx} {plan_type} {yyyy} {group} {label} {id}"
+    # scripts-1time/duplicate_map.sh NC Congress 2022 Proportional PG-NOTABLE 7cfcec86-b8bf-41fa-a6c2-0be9b3799747
+    return (
+        f"scripts-1time/duplicate_map.sh {xx} {plan_type} {yyyy} {group} {label} {id}"
+    )
 
 
-for xx, maps in notable_maps.items():
-    for dim, id in maps.items():
-        os.system(make_command(dim.capitalize(), label, xx, id))
-        pass
+print("#!/bin/bash")
+print("# Duplicate notable maps in DRA.")
+for xx, plan_types in NOTABLE_MAPS.items():
+    for plan_type, notable_maps in plan_types.items():
+        if plan_type == "congress":
+            continue  # already duplicated
+        for dim, guid in notable_maps.items():
+            print(make_command(dim.capitalize(), label, xx, guid))
+            #  os.system(make_command(dim.capitalize(), label, xx, id))
 
-#
+pass
--- scripts-1time/duplicate_official_maps.py
+++ scripts-1time/duplicate_official_maps.py
@@ -5,15 +5,13 @@ Create a shell script to uplicate Official maps in DRA.
 
 For example:
 
-$ scripts/duplicate_official_maps.py
+$ scripts-1time/duplicate_official_maps.py
 
 For documentation, type:
 
-$ scripts/duplicate_official_maps.py -h
+$ scripts-1time/duplicate_official_maps.py -h
 
 """
-
-import os
 
 from rdabase import plan_type, yyyy
 from constants import OFFICIAL_MAPS
@@ -23,7 +21,7 @@ label: str = "PG-" + group.upper()
 
 
 def make_command(group: str, label: str, xx: str, id: str) -> str:
-    # scripts/duplicate_map.sh NC Congress 2022 Official PG-OFFICIAL c62fa27b-1f4b-40ba-bcd7-9e2ca6f9df87
+    # scripts-1time/duplicate_map.sh NC Congress 2022 Official PG-OFFICIAL c62fa27b-1f4b-40ba-bcd7-9e2ca6f9df87
     return (
         f"scripts-1time/duplicate_map.sh {xx} {plan_type} {yyyy} {group} {label} {id}"
     )
