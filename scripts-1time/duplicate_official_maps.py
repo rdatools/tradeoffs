d16de2fbd5d64e59f57cd0b5fb368bb67e17f616
alecramsay <4155251+alecramsay@users.noreply.github.com>
2024-09-24 06:44:24 -0700

    WIP

--- scripts-1time/duplicate_official_maps.py
+++ scripts-1time/duplicate_official_maps.py
@@ -7,20 +7,27 @@ For example:
 
 $ scripts-1time/duplicate_official_maps.py
 
+Remember to start a server. Open a new Terminal and type:
+
+$ cd dra
+$ npm run test
+
+Then open a browser and go to http://localhost:3000/.
+
 For documentation, type:
 
 $ scripts-1time/duplicate_official_maps.py -h
 
 """
 
-from rdabase import plan_type, yyyy
+from rdabase import yyyy
 from constants import SAMPLE_STATES, OFFICIAL_MAPS
 
 group: str = "Official"
 label: str = "PG-" + group.upper()
 
 
-def make_command(group: str, label: str, xx: str, id: str) -> str:
+def make_command(xx: str, plan_type: str, group: str, label: str, id: str) -> str:
     # scripts-1time/duplicate_map.sh NC Congress 2022 Official PG-OFFICIAL c62fa27b-1f4b-40ba-bcd7-9e2ca6f9df87
     return (
         f"scripts-1time/duplicate_map.sh {xx} {plan_type} {yyyy} {group} {label} {id}"
@@ -29,14 +36,19 @@ def make_command(group: str, label: str, xx: str, id: str) -> str:
 
 print("#!/bin/bash")
 print("# Duplicate Official maps in DRA.")
+print("# Remember to start the DRA server before running these commands.")
 for xx, guids in OFFICIAL_MAPS.items():
     for plan_type, guid in guids.items():
         if plan_type == "congress":
             continue  # already duplicated
         if xx in SAMPLE_STATES:
             continue  # already duplicated
-        print(make_command(group, label, xx, guid))
-        # os.system(make_command(group, label, xx, id))
+
+        log_file: str = f"intermediate/{xx}_{yyyy}_{plan_type}_duplicate.log"
+        command: str = (
+            make_command(xx, plan_type, group, label, guid) + f" > {log_file}"
+        )
+        print(command)
 
 pass
 
--- scripts-1time/duplicate_official_maps.sh
+++ scripts-1time/duplicate_official_maps.sh
@@ -1,67 +1,68 @@
 #!/bin/bash
 # Duplicate Official maps in DRA.
-scripts-1time/duplicate_map.sh AL lower 2022 Official PG-OFFICIAL 642e8d2a-248c-4191-a88a-a6cdad508cdb
-scripts-1time/duplicate_map.sh AL upper 2022 Official PG-OFFICIAL b694ea80-20d6-45fb-a0e2-09ddaa2880d5
-scripts-1time/duplicate_map.sh AZ upper 2022 Official PG-OFFICIAL 72e34ca5-9ae1-4ddd-96f0-697bd261294e
-scripts-1time/duplicate_map.sh AR lower 2022 Official PG-OFFICIAL 583730d5-1254-4dca-8f73-850940804307
-scripts-1time/duplicate_map.sh AR upper 2022 Official PG-OFFICIAL 083cee53-0d0c-43af-ace1-863ccd3a2fe5
-scripts-1time/duplicate_map.sh CA lower 2022 Official PG-OFFICIAL 43cea5ab-ef0a-4946-b3e2-c3f2aaf73cf1
-scripts-1time/duplicate_map.sh CA upper 2022 Official PG-OFFICIAL f0a4ed9d-205f-4122-abab-fa3c75b6b050
-scripts-1time/duplicate_map.sh CO lower 2022 Official PG-OFFICIAL dee1f8ad-23c1-497b-859c-6d1a9293bbd5
-scripts-1time/duplicate_map.sh CO upper 2022 Official PG-OFFICIAL 9b5ec272-1fba-4a7a-ac6c-f2dcb4c6d0ae
-scripts-1time/duplicate_map.sh CT lower 2022 Official PG-OFFICIAL 2e38109d-9987-46e3-9ff0-dd6c3caa59e8
-scripts-1time/duplicate_map.sh CT upper 2022 Official PG-OFFICIAL fc9e83c0-59e2-4dea-b0cf-d2fff1c0b8a3
-scripts-1time/duplicate_map.sh FL lower 2022 Official PG-OFFICIAL 488c9284-a2c4-4846-9d7d-08af9d98a6c5
-scripts-1time/duplicate_map.sh FL upper 2022 Official PG-OFFICIAL 91f2ce50-b3c1-4d74-87f2-d3a9fa078129
-scripts-1time/duplicate_map.sh GA lower 2022 Official PG-OFFICIAL 041f73c7-8f4f-48c5-a5ff-cb059e692827
-scripts-1time/duplicate_map.sh GA upper 2022 Official PG-OFFICIAL f1249844-4b84-45e6-9ca6-e13f6ddddb0d
-scripts-1time/duplicate_map.sh ID upper 2022 Official PG-OFFICIAL a56da372-79a4-48e7-9fd5-b689d0ccb602
-scripts-1time/duplicate_map.sh IN lower 2022 Official PG-OFFICIAL db849782-86e9-4293-a492-3e17d7cfceab
-scripts-1time/duplicate_map.sh IN upper 2022 Official PG-OFFICIAL f2d2e4a3-f50d-4f77-bc5d-027ba0e2717e
-scripts-1time/duplicate_map.sh IA lower 2022 Official PG-OFFICIAL 3bba1647-f4a4-4374-8021-a8e4aeb6f15a
-scripts-1time/duplicate_map.sh IA upper 2022 Official PG-OFFICIAL 0e81cf3d-648d-4851-b14b-0ee893f92610
-scripts-1time/duplicate_map.sh KS lower 2022 Official PG-OFFICIAL 781f29f1-259e-4235-8b14-85137bc38893
-scripts-1time/duplicate_map.sh KS upper 2022 Official PG-OFFICIAL 2bc2f97f-a285-4596-9fcd-6108056b229f
-scripts-1time/duplicate_map.sh KY lower 2022 Official PG-OFFICIAL 8eed4db7-46cd-4e09-a179-c0a7e821a963
-scripts-1time/duplicate_map.sh KY upper 2022 Official PG-OFFICIAL 8ef6d39b-ae52-4587-8ed1-88b781dd4300
-scripts-1time/duplicate_map.sh LA lower 2022 Official PG-OFFICIAL d63b737c-a8b3-46e9-8855-aa20a728c2b5
-scripts-1time/duplicate_map.sh LA upper 2022 Official PG-OFFICIAL 12eedba5-68de-4ab4-a3bb-7f59d9268041
-scripts-1time/duplicate_map.sh MA lower 2022 Official PG-OFFICIAL f4bfa2f3-1e73-4e0b-a237-cc2aa2e611b8
-scripts-1time/duplicate_map.sh MA upper 2022 Official PG-OFFICIAL b32decd8-3ed9-4ab2-876b-c92cfa798e33
-scripts-1time/duplicate_map.sh MI lower 2022 Official PG-OFFICIAL 8f2d562d-9d7f-4d3b-992e-5c52debd8f08
-scripts-1time/duplicate_map.sh MI upper 2022 Official PG-OFFICIAL 7563fcf7-de01-411a-aa51-c56cf76340b0
-scripts-1time/duplicate_map.sh MN lower 2022 Official PG-OFFICIAL 0e589052-0df8-4b8a-bede-c8e9fcc8814c
-scripts-1time/duplicate_map.sh MN upper 2022 Official PG-OFFICIAL 8d55ed9f-d825-4a8a-95e4-864cbeede612
-scripts-1time/duplicate_map.sh MS lower 2022 Official PG-OFFICIAL f8554517-7286-461a-9cfa-892b3d4731f7
-scripts-1time/duplicate_map.sh MS upper 2022 Official PG-OFFICIAL f3908ec6-949f-444b-9fd5-7b2173abcb6e
-scripts-1time/duplicate_map.sh MO lower 2022 Official PG-OFFICIAL ebb632d0-2569-4a2a-81f6-1d8d6adc0d5e
-scripts-1time/duplicate_map.sh MO upper 2022 Official PG-OFFICIAL 8d25c96a-2a78-40b2-b2f0-c504ecaa6762
-scripts-1time/duplicate_map.sh NE upper 2022 Official PG-OFFICIAL ab1f2fef-b979-4431-844a-98d93a4d6730
-scripts-1time/duplicate_map.sh NH lower 2022 Official PG-OFFICIAL dfddaed1-5a76-4ca7-8a11-ddc533ea9f6e
-scripts-1time/duplicate_map.sh NH upper 2022 Official PG-OFFICIAL 15bf8903-0c56-4536-9265-7b7177726a73
-scripts-1time/duplicate_map.sh NV lower 2022 Official PG-OFFICIAL 2a33203f-8725-48dc-ba1b-478bd6012a58
-scripts-1time/duplicate_map.sh NV upper 2022 Official PG-OFFICIAL 99808524-5012-4119-91ce-973ba647806b
-scripts-1time/duplicate_map.sh NJ upper 2022 Official PG-OFFICIAL 61388384-5d1f-4f3b-9669-31e485b781f1
-scripts-1time/duplicate_map.sh NM lower 2022 Official PG-OFFICIAL 5e00bd63-7008-4b5e-b570-db5ba1d36785
-scripts-1time/duplicate_map.sh NM upper 2022 Official PG-OFFICIAL 11c9cc63-8521-4785-89f3-58b5e4359507
-scripts-1time/duplicate_map.sh NY lower 2022 Official PG-OFFICIAL 1933abca-db19-40f9-808d-17dd90642cdc
-scripts-1time/duplicate_map.sh NY upper 2022 Official PG-OFFICIAL bb78268d-ea3c-46a1-a484-c192a6c0e15d
-scripts-1time/duplicate_map.sh OH lower 2022 Official PG-OFFICIAL 90de759b-f81d-44c0-a019-d2714a38cc3a
-scripts-1time/duplicate_map.sh OH upper 2022 Official PG-OFFICIAL 6eba71b1-18a1-48d1-99c9-62afba432253
-scripts-1time/duplicate_map.sh OK lower 2022 Official PG-OFFICIAL a4a178b9-581b-4591-bcf9-13e18bd850ea
-scripts-1time/duplicate_map.sh OK upper 2022 Official PG-OFFICIAL 0fa6fe25-9db7-4777-a150-d2309188823f
-scripts-1time/duplicate_map.sh OR lower 2022 Official PG-OFFICIAL c572dfa0-276b-4a76-8531-06669d885f27
-scripts-1time/duplicate_map.sh OR upper 2022 Official PG-OFFICIAL a5aadfab-0234-489a-a4df-cc008a844926
-scripts-1time/duplicate_map.sh RI lower 2022 Official PG-OFFICIAL 90ec6599-f715-4a3a-b50b-5858788046ac
-scripts-1time/duplicate_map.sh RI upper 2022 Official PG-OFFICIAL 2aab93a2-7e15-4047-87a8-02b0deb3eb4e
-scripts-1time/duplicate_map.sh TN lower 2022 Official PG-OFFICIAL c0ed8258-ce44-4eec-9736-2cc3b86a9d16
-scripts-1time/duplicate_map.sh TN upper 2022 Official PG-OFFICIAL 62c62bed-a104-4afc-9286-9b3915b45f58
-scripts-1time/duplicate_map.sh UT lower 2022 Official PG-OFFICIAL 927e8547-08f9-43e3-8abc-68a7faf89d71
-scripts-1time/duplicate_map.sh UT upper 2022 Official PG-OFFICIAL cfd457c9-6d3e-496d-9377-4b68de384a57
-scripts-1time/duplicate_map.sh VA lower 2022 Official PG-OFFICIAL fa427b6b-e057-43e9-8000-9d1ae319870e
-scripts-1time/duplicate_map.sh VA upper 2022 Official PG-OFFICIAL 1b489a5a-f457-428d-b552-9ede8ceb9b99
-scripts-1time/duplicate_map.sh WA upper 2022 Official PG-OFFICIAL 3e3c5f5c-3a83-4847-b1d8-5328fb3b9e31
-scripts-1time/duplicate_map.sh WI lower 2022 Official PG-OFFICIAL 6a8a362d-0c59-4d81-aea3-28cba004b502
-scripts-1time/duplicate_map.sh WI upper 2022 Official PG-OFFICIAL cb8ed0b5-013f-4b1a-ba49-1b03445416c9
-scripts-1time/duplicate_map.sh WV lower 2022 Official PG-OFFICIAL bd6eb9ac-ac94-465f-82d6-4e086307d5e5
-scripts-1time/duplicate_map.sh WV upper 2022 Official PG-OFFICIAL c3ca956f-0b1d-4750-894d-b2e9dadc642c
+# Remember to start the DRA server before running these commands.
+scripts-1time/duplicate_map.sh AL lower 2022 Official PG-OFFICIAL 642e8d2a-248c-4191-a88a-a6cdad508cdb > intermediate/AL_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh AL upper 2022 Official PG-OFFICIAL b694ea80-20d6-45fb-a0e2-09ddaa2880d5 > intermediate/AL_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh AZ upper 2022 Official PG-OFFICIAL 72e34ca5-9ae1-4ddd-96f0-697bd261294e > intermediate/AZ_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh AR lower 2022 Official PG-OFFICIAL 583730d5-1254-4dca-8f73-850940804307 > intermediate/AR_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh AR upper 2022 Official PG-OFFICIAL 083cee53-0d0c-43af-ace1-863ccd3a2fe5 > intermediate/AR_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh CA lower 2022 Official PG-OFFICIAL 43cea5ab-ef0a-4946-b3e2-c3f2aaf73cf1 > intermediate/CA_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh CA upper 2022 Official PG-OFFICIAL f0a4ed9d-205f-4122-abab-fa3c75b6b050 > intermediate/CA_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh CO lower 2022 Official PG-OFFICIAL dee1f8ad-23c1-497b-859c-6d1a9293bbd5 > intermediate/CO_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh CO upper 2022 Official PG-OFFICIAL 9b5ec272-1fba-4a7a-ac6c-f2dcb4c6d0ae > intermediate/CO_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh CT lower 2022 Official PG-OFFICIAL 2e38109d-9987-46e3-9ff0-dd6c3caa59e8 > intermediate/CT_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh CT upper 2022 Official PG-OFFICIAL fc9e83c0-59e2-4dea-b0cf-d2fff1c0b8a3 > intermediate/CT_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh FL lower 2022 Official PG-OFFICIAL 488c9284-a2c4-4846-9d7d-08af9d98a6c5 > intermediate/FL_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh FL upper 2022 Official PG-OFFICIAL 91f2ce50-b3c1-4d74-87f2-d3a9fa078129 > intermediate/FL_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh GA lower 2022 Official PG-OFFICIAL 041f73c7-8f4f-48c5-a5ff-cb059e692827 > intermediate/GA_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh GA upper 2022 Official PG-OFFICIAL f1249844-4b84-45e6-9ca6-e13f6ddddb0d > intermediate/GA_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh ID upper 2022 Official PG-OFFICIAL a56da372-79a4-48e7-9fd5-b689d0ccb602 > intermediate/ID_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh IN lower 2022 Official PG-OFFICIAL db849782-86e9-4293-a492-3e17d7cfceab > intermediate/IN_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh IN upper 2022 Official PG-OFFICIAL f2d2e4a3-f50d-4f77-bc5d-027ba0e2717e > intermediate/IN_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh IA lower 2022 Official PG-OFFICIAL 3bba1647-f4a4-4374-8021-a8e4aeb6f15a > intermediate/IA_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh IA upper 2022 Official PG-OFFICIAL 0e81cf3d-648d-4851-b14b-0ee893f92610 > intermediate/IA_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh KS lower 2022 Official PG-OFFICIAL 781f29f1-259e-4235-8b14-85137bc38893 > intermediate/KS_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh KS upper 2022 Official PG-OFFICIAL 2bc2f97f-a285-4596-9fcd-6108056b229f > intermediate/KS_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh KY lower 2022 Official PG-OFFICIAL 8eed4db7-46cd-4e09-a179-c0a7e821a963 > intermediate/KY_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh KY upper 2022 Official PG-OFFICIAL 8ef6d39b-ae52-4587-8ed1-88b781dd4300 > intermediate/KY_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh LA lower 2022 Official PG-OFFICIAL d63b737c-a8b3-46e9-8855-aa20a728c2b5 > intermediate/LA_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh LA upper 2022 Official PG-OFFICIAL 12eedba5-68de-4ab4-a3bb-7f59d9268041 > intermediate/LA_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh MA lower 2022 Official PG-OFFICIAL f4bfa2f3-1e73-4e0b-a237-cc2aa2e611b8 > intermediate/MA_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh MA upper 2022 Official PG-OFFICIAL b32decd8-3ed9-4ab2-876b-c92cfa798e33 > intermediate/MA_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh MI lower 2022 Official PG-OFFICIAL 8f2d562d-9d7f-4d3b-992e-5c52debd8f08 > intermediate/MI_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh MI upper 2022 Official PG-OFFICIAL 7563fcf7-de01-411a-aa51-c56cf76340b0 > intermediate/MI_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh MN lower 2022 Official PG-OFFICIAL 0e589052-0df8-4b8a-bede-c8e9fcc8814c > intermediate/MN_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh MN upper 2022 Official PG-OFFICIAL 8d55ed9f-d825-4a8a-95e4-864cbeede612 > intermediate/MN_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh MS lower 2022 Official PG-OFFICIAL f8554517-7286-461a-9cfa-892b3d4731f7 > intermediate/MS_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh MS upper 2022 Official PG-OFFICIAL f3908ec6-949f-444b-9fd5-7b2173abcb6e > intermediate/MS_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh MO lower 2022 Official PG-OFFICIAL ebb632d0-2569-4a2a-81f6-1d8d6adc0d5e > intermediate/MO_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh MO upper 2022 Official PG-OFFICIAL 8d25c96a-2a78-40b2-b2f0-c504ecaa6762 > intermediate/MO_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh NE upper 2022 Official PG-OFFICIAL ab1f2fef-b979-4431-844a-98d93a4d6730 > intermediate/NE_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh NH lower 2022 Official PG-OFFICIAL dfddaed1-5a76-4ca7-8a11-ddc533ea9f6e > intermediate/NH_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh NH upper 2022 Official PG-OFFICIAL 15bf8903-0c56-4536-9265-7b7177726a73 > intermediate/NH_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh NV lower 2022 Official PG-OFFICIAL 2a33203f-8725-48dc-ba1b-478bd6012a58 > intermediate/NV_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh NV upper 2022 Official PG-OFFICIAL 99808524-5012-4119-91ce-973ba647806b > intermediate/NV_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh NJ upper 2022 Official PG-OFFICIAL 61388384-5d1f-4f3b-9669-31e485b781f1 > intermediate/NJ_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh NM lower 2022 Official PG-OFFICIAL 5e00bd63-7008-4b5e-b570-db5ba1d36785 > intermediate/NM_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh NM upper 2022 Official PG-OFFICIAL 11c9cc63-8521-4785-89f3-58b5e4359507 > intermediate/NM_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh NY lower 2022 Official PG-OFFICIAL 1933abca-db19-40f9-808d-17dd90642cdc > intermediate/NY_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh NY upper 2022 Official PG-OFFICIAL bb78268d-ea3c-46a1-a484-c192a6c0e15d > intermediate/NY_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh OH lower 2022 Official PG-OFFICIAL 90de759b-f81d-44c0-a019-d2714a38cc3a > intermediate/OH_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh OH upper 2022 Official PG-OFFICIAL 6eba71b1-18a1-48d1-99c9-62afba432253 > intermediate/OH_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh OK lower 2022 Official PG-OFFICIAL a4a178b9-581b-4591-bcf9-13e18bd850ea > intermediate/OK_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh OK upper 2022 Official PG-OFFICIAL 0fa6fe25-9db7-4777-a150-d2309188823f > intermediate/OK_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh OR lower 2022 Official PG-OFFICIAL c572dfa0-276b-4a76-8531-06669d885f27 > intermediate/OR_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh OR upper 2022 Official PG-OFFICIAL a5aadfab-0234-489a-a4df-cc008a844926 > intermediate/OR_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh RI lower 2022 Official PG-OFFICIAL 90ec6599-f715-4a3a-b50b-5858788046ac > intermediate/RI_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh RI upper 2022 Official PG-OFFICIAL 2aab93a2-7e15-4047-87a8-02b0deb3eb4e > intermediate/RI_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh TN lower 2022 Official PG-OFFICIAL c0ed8258-ce44-4eec-9736-2cc3b86a9d16 > intermediate/TN_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh TN upper 2022 Official PG-OFFICIAL 62c62bed-a104-4afc-9286-9b3915b45f58 > intermediate/TN_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh UT lower 2022 Official PG-OFFICIAL 927e8547-08f9-43e3-8abc-68a7faf89d71 > intermediate/UT_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh UT upper 2022 Official PG-OFFICIAL cfd457c9-6d3e-496d-9377-4b68de384a57 > intermediate/UT_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh VA lower 2022 Official PG-OFFICIAL fa427b6b-e057-43e9-8000-9d1ae319870e > intermediate/VA_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh VA upper 2022 Official PG-OFFICIAL 1b489a5a-f457-428d-b552-9ede8ceb9b99 > intermediate/VA_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh WA upper 2022 Official PG-OFFICIAL 3e3c5f5c-3a83-4847-b1d8-5328fb3b9e31 > intermediate/WA_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh WI lower 2022 Official PG-OFFICIAL 6a8a362d-0c59-4d81-aea3-28cba004b502 > intermediate/WI_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh WI upper 2022 Official PG-OFFICIAL cb8ed0b5-013f-4b1a-ba49-1b03445416c9 > intermediate/WI_2022_upper_duplicate.log
+scripts-1time/duplicate_map.sh WV lower 2022 Official PG-OFFICIAL bd6eb9ac-ac94-465f-82d6-4e086307d5e5 > intermediate/WV_2022_lower_duplicate.log
+scripts-1time/duplicate_map.sh WV upper 2022 Official PG-OFFICIAL c3ca956f-0b1d-4750-894d-b2e9dadc642c > intermediate/WV_2022_upper_duplicate.log
