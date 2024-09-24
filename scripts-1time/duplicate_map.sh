9822a0bdc42857609de36dca32b71738f7d00e3a
alecramsay <4155251+alecramsay@users.noreply.github.com>
2024-09-24 11:26:02 -0700

    WIP

--- scripts-1time/duplicate_map.sh
+++ scripts-1time/duplicate_map.sh
@@ -7,6 +7,9 @@
 # scripts/duplicate_map.sh NC Congress 2022 Official PG-OFFICIAL c62fa27b-1f4b-40ba-bcd7-9e2ca6f9df87
 # scripts/duplicate_map.sh NC Congress 2022 Proportional PG-NOTABLE 7cfcec86-b8bf-41fa-a6c2-0be9b3799747
 #
+# The 'id' for the "Trade-offs" groups is "5004e36d-c772-4bcc-a296-7ea27ab766c6".
+# dumpdb.js -t groups | grep Trade-offs
+#
 
 XX=$1
 PLAN_TYPE=$2
@@ -24,6 +27,6 @@ PW=Front.Seat # TODO: Remove this! - This doesn't seem to work anymore - 07/04/2
 NAME="$XX $YYYY $PLAN_TYPE - $DIM"
 DESC="Copy of $XX $DIM"
 
-echo $SCRIPT_DIR/draclient.js -u $USER -x $PW -i $ID -d -N "$NAME" -D "$DESC" -L $LABEL TRADEOFFS
-$SCRIPT_DIR/draclient.js  -u $USER -x $PW -i $ID -d -N "$NAME" -D "$DESC" -L $LABEL TRADEOFFS
+echo $SCRIPT_DIR/draclient.js -u $USER -x $PW -i $ID -d -N "$NAME" -D "$DESC" -L $LABEL TRADEOFFS -G 5004e36d-c772-4bcc-a296-7ea27ab766c6
+$SCRIPT_DIR/draclient.js  -u $USER -x $PW -i $ID -d -N "$NAME" -D "$DESC" -L $LABEL TRADEOFFS -G 5004e36d-c772-4bcc-a296-7ea27ab766c6
 
