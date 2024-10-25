10f480191bb55f577be5593e4961e7cba8859bc9
alecramsay <4155251+alecramsay@users.noreply.github.com>
2024-10-25 07:55:51 -0700

    Updated setup_state.sh script

--- scripts/setup_state.sh
+++ scripts/setup_state.sh
@@ -4,21 +4,19 @@
 #
 # For example:
 #
-# scripts/SETUP.sh NC
+# scripts/setup_state.sh NC
 #
 
 XX=$1
 ROOT_DIR=../../iCloud/fileout/tradeoffs
 
 mkdir $ROOT_DIR/$XX
-# mkdir $ROOT_DIR/$XX/data
+mkdir $ROOT_DIR/$XX/data
 mkdir $ROOT_DIR/$XX/docs
 mkdir $ROOT_DIR/$XX/ensembles
-mkdir $ROOT_DIR/$XX/ensembles-upper
-mkdir $ROOT_DIR/$XX/ensembles-lower
-# mkdir $ROOT_DIR/$XX/plans
-# mkdir $ROOT_DIR/$XX/jobs
-# mkdir $ROOT_DIR/$XX/pushed
+mkdir $ROOT_DIR/$XX/plans
+mkdir $ROOT_DIR/$XX/jobs
+mkdir $ROOT_DIR/$XX/pushed
 
 mkdir $ROOT_DIR/$XX/docs/_data
 mkdir $ROOT_DIR/$XX/docs/assets
